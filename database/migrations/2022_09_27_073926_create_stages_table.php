457e35082529b585a938aed808923c42e800feac
lion-rion <lionact08@gmail.com>
2022-09-27 18:04:24 +0900

    ✅add : stage migration

---
+++ database/migrations/2022_09_27_073926_create_stages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->boolean('stage1-1');
+            $table->foreignId('user_id')->constrained(); //ユーザーID追加
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stages');
+    }
+};
